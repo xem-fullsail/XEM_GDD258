7feca6aefd97ff7e70da3a723e885ac7db145a72
xem <kmoniz@student.fullsail.edu>
2022-05-11 14:05:06 -0400

    Add Comet and people creation menu

--- Lectures/Person/Person.cpp
+++ Lectures/Person/Person.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <format>
+#include <vector>
 
 
 class Person {
@@ -35,6 +36,19 @@ public:
 
 };
 
+class Comet {
+
+public:
+    std::string Name;
+
+    Comet();
+
+    Comet(std::string name) {
+        Name = name;
+    }
+
+};
+
 std::string GetTextInput(std::string prompt) {
     std::string s;
     std::cout << prompt << " ";
@@ -42,8 +56,28 @@ std::string GetTextInput(std::string prompt) {
     return s;
 }
 
-int main()
-{
+int GetIntInput(const char* message, int min = 0, int max = 0) {
+    int value;
+    while (true) {
+        std::cout << message;
+        std::cin >> value;
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            std::cout << "Invalid integer.\n";
+        }
+        else {
+            std::cin.clear();
+            std::cin.ignore(INT_MAX, '\n');
+            if (min == 0 && max == 0) break;
+            else if (value < min || value > max) std::cout << "Input should be between " << min << " and " << max << ".\n";
+            else break;
+        }
+    }
+    return value;
+}
+
+Person CreatePerson() {
     Person person;
     person.FirstName = GetTextInput("What is your first name?");
     person.LastName = GetTextInput("What is your last name?");
@@ -51,6 +85,45 @@ int main()
     person.City = GetTextInput("What is your city?");
     person.State = GetTextInput("What is your state?");
     person.ZipCode = std::stoi(GetTextInput("What is your zip code?"));
-    std::cout << person.Display();
+    return person;
+}
+
+int main()
+{
+    std::vector<Person> people;
+
+    bool cont = true;
+    while (cont) {
+        int numPeople = 0;
+        
+        std::cout << "+------------------------------------------------+\n1. Create a person\n2. Create people\n0. Quit\n";
+        int menuChoice = GetIntInput("What would you like to do ? ", 0, 2);
+
+        switch (menuChoice) {
+            case 1:
+                std::cout << "+------------------------------------------------+\n";
+                people.push_back(CreatePerson());
+                break;
+            case 2:
+                numPeople = GetIntInput("How many people would you like to create? ");
+                for (int i = 0; i < numPeople; i++) {
+                    std::cout << "+------------------------------------------------+\n";
+                    people.push_back(CreatePerson());
+                }
+                break;
+            default:
+                cont = false;
+                break;
+        }
+    }
+
+    std::cout << "+------------------------------------------------+\nCurrent People:\n\n";
+    for (Person p : people)
+        std::cout << p.Display() << std::endl;
+
+    Comet comet = Comet("Sozen's Comet");
+    std::cout << "\n+------------------------------------------------+\Current Comets:\n\n";
+    std::cout << comet.Name << std::endl;
+
 }
 
